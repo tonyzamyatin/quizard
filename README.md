d5a8c7c084f865f65f09bac5b71b169a41e343b7
Florian Kircher <fkircher11@gmail.com>
2023-08-06 16:41:10 +0200

    Create initial poc to send requests to openai

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # quizard-poc
+Quizard aims to generate flashcards using LLMs
+This is the poc to test the openai model performance
+And also a playground to test various techniques
 ## environment
 The api key can be set in the .env file
 
--- main.py
+++ main.py
@@ -2,8 +2,45 @@ import os
 import openai
 from dotenv import load_dotenv
 
+
+def read_file(path) -> str:
+    f = open(path, "r")
+    content = f.read()
+    f.close()
+    return content
+
 # load the openai api key from .env file
 load_dotenv('.env')
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-print(openai.Model.list())
+# print(openai.Model.list())
+example_system_prompt = read_file('example_system_prompt.txt')
+example_user_input = read_file('example_user_input.txt')
+example_response = read_file('example_response.txt')
+
+system_prompt = read_file('system_prompt.txt') # Generiere 5 flashcards ...
+user_input = read_file('user_input.txt') # Ausgangstext
+
+messages = [
+    {"role": "user", "content": example_user_input},
+    {"role": "system", "content": example_system_prompt},
+    {"role": "assistant", "content": example_response},
+
+    {"role": "user", "content": user_input},
+    {"role": "system", "content": system_prompt}
+]
+completion35 = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages, max_tokens=1000)
+
+print("---")
+print(completion35)
+print(completion35.response_ms)
+text: str = completion35.choices[0].message.content
+
+f = open('response.txt', 'w')
+
+for e in text.split('\n'):
+    print(e)
+    print(e.split(';'))
+    f.write(e)
+    f.write('\n')
+f.close()
