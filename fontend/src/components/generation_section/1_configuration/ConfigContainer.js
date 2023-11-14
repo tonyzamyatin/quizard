5035d945fcce70d3f547b8e7213d4e8619499d83
tonyzamyatin <anton.zamyatin14@gmail.com>
2023-11-14 18:43:17 +0100

    Added updateProgress skeleton and a "choose export format" dropdown to UI

--- fontend/src/components/generation_section/1_configuration/ConfigContainer.js
+++ fontend/src/components/generation_section/1_configuration/ConfigContainer.js
@@ -2,7 +2,7 @@ import CTAButton from '../../global/CTAButton';
 import ConfigMenu from "./ConfigMenu";
 import GenerationSteps from "../../global/GenerationSteps";
 
-function ConfigContainer({setGenerationStep, lang, setLang, mode, setMode}) {
+function ConfigContainer({setGenerationStep, lang, setLang, mode, setMode, exportFormat, setExportFormat}) {
 
     function handleNextClick() {
         setGenerationStep(GenerationSteps.TEXT_UPLOAD);
@@ -12,10 +12,11 @@ function ConfigContainer({setGenerationStep, lang, setLang, mode, setMode}) {
         <div className="generation-section-container config-container">
             <h2>Configure the Flashcard Generator</h2>
             <div className="generation-section-box">
-                <ConfigMenu lang={lang} setLang={setLang} mode={mode} setMode={setMode}/>
+                <ConfigMenu lang={lang} setLang={setLang} mode={mode} setMode={setMode} exportFormat={exportFormat} setExportFormat={setExportFormat}/>
             </div>
             <div className="button-area">
-                <CTAButton buttonName="Next" onButtonClick={handleNextClick} active={lang.length > 0 && mode.length > 0}/>
+                {/*TODO: Potentially dangerous condition in 'active={...}'. Susceptible to attack?*/}
+                <CTAButton buttonName="Next" onButtonClick={handleNextClick} active={lang.length > 0 && mode.length > 0 && exportFormat.length > 0}/>
             </div>
         </div>
     )
--- fontend/src/components/generation_section/1_configuration/ConfigMenu.js
+++ fontend/src/components/generation_section/1_configuration/ConfigMenu.js
@@ -1,22 +1,29 @@
 import React, {useEffect} from "react";
 import Dropdown from "../../global/Dropdown";
 
-function ConfigMenu({ lang, setLang, mode, setMode }) {
+// TODO: Pep up config menu by using icons and make the configuration process more user friendly
+function ConfigMenu({ lang, setLang, mode, setMode, exportFormat, setExportFormat}) {
     const languageOptions = ["English", "German"];
     const modeOptions = ["Practice", "Test", "Cloze"];
-
-    const handleModeChange = (event) => {
-        setMode(event.target.value);
-    };
+    const exportOptions = ["CSV", "Anki"]
 
     const handleLangChange = (event) => {
         setLang(event.target.value);
     };
 
+    const handleModeChange = (event) => {
+        setMode(event.target.value);
+    };
+
+    const handleExportFormatChange = (event) => {
+        setExportFormat(event.target.value);
+    }
+
     return (
         <div className="config-menu">
             <Dropdown labelText="Choose your language" selected={lang} options={languageOptions} onChange={handleLangChange}/>
-            <Dropdown labelText="Choose your generation mode" selected={mode} options={modeOptions} onChange={handleModeChange}/>
+            <Dropdown labelText="Choose generation mode" selected={mode} options={modeOptions} onChange={handleModeChange}/>
+            <Dropdown labelText="Choose export format " selected={exportFormat} options={exportOptions} onChange={handleExportFormatChange}/>
         </div>
     );
 }
